6791e5e441ceaec47d72a8b82eb94e3f5ead6c59
Anton Sustavov <sustav86@gmail.com>
2020-03-15 01:10:59 +0200

    add hibernate

---
+++ src/test/java/com/marcobehler/part_03_spring/code/TransactionalTestsExercise.java
@@ -0,0 +1,130 @@
+package com.marcobehler.part_03_spring.code;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * @author Anton Sustavov
+ * @since 2020/03/13
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(
+        classes = TransactionalTestsExercise.MySpringConfig.class)
+@SuppressWarnings("Duplicates") // for IntelliJ idea only
+public class TransactionalTestsExercise {
+    @Autowired
+    private BankTeller teller;
+    @Autowired
+    private DataSource ds;
+
+    @Before
+    public void setUp() {
+        try (Connection connection = ds.getConnection()) {
+            createTables(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+//    @Transactional
+    public void exercise() throws SQLException {
+        System.out.println("This test is transactional...");
+        Long balance = teller.getAccountBalance("Donald Trump");
+        assertThat(balance, equalTo(Long.MAX_VALUE));
+    }
+
+    public static class BankTeller {
+        @Autowired
+        private DataSource ds;
+        @Resource(name = "bankTeller")
+        private BankTeller self;
+        // will this roll back in a @Transactinal test? YES!
+        @Transactional(propagation = Propagation.REQUIRED)
+        public Long getAccountBalance(String name) {
+            // let's return the balance from a database table
+            Long balance = new JdbcTemplate(ds).queryForObject(
+                    "select balance from accounts " +
+                            "where name = ?", Long.class, name);
+            System.out.println("The balance for : " + name + " is: " +
+                    balance);
+            // this call will open up another connection/transaction!
+            self.audit(name, "Get Account Balance");
+            return balance;
+        }
+        // will this roll back in a @Transactional test? NO!
+        @Transactional(propagation = Propagation.REQUIRES_NEW)
+        public void audit(final String name, final String activity) {
+            MapSqlParameterSource params =
+                    new MapSqlParameterSource("name", name);
+            params.addValue("date_occurred", new Date());
+            params.addValue("description", activity);
+            new SimpleJdbcInsert(ds).withTableName("account_activity")
+                    .execute(params);
+        }
+    }
+
+    private void createTables(Connection conn) {
+        try {
+            conn.createStatement().execute("create table if not exists " +
+                    "account_activity "
+                    + "(date_occurred date, account_holder VARCHAR," +
+                    " description VARCHAR)");
+            conn.createStatement().execute("create table if not exists " +
+                    "accounts "
+                    + "(name varchar primary key, balance bigint)");
+            conn.createStatement().execute("insert into accounts values"
+                    + "('Donald Trump'," + Long.MAX_VALUE + ")");
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Configuration
+    @EnableTransactionManagement(proxyTargetClass = true)
+    public static class MySpringConfig {
+        @Bean(name = "bankTeller")
+        public BankTeller teller() {
+            return new BankTeller();
+        }
+        @Bean
+        public DataSource dataSource() {
+            JdbcDataSource ds = new JdbcDataSource();
+            ds.setURL("jdbc:h2:mem:exercise_db;DB_CLOSE_DELAY=-1");
+            ds.setUser("sa");
+            ds.setPassword("sa");
+            return ds;
+        }
+        @Bean
+        public PlatformTransactionManager txManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+    }
+}
